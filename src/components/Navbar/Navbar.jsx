6add5dc2ab308b8169ad25df8cff9353b65977b7
Arun-ez <arunshaw433@gmail.com>
2023-03-17 03:01:02 +0530

    product path added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import "./Navbar.css"
 import React from 'react'
-import { Flex, Image, useDisclosure } from '@chakra-ui/react';
+import { Flex, useDisclosure } from '@chakra-ui/react';
 import { Input } from '@chakra-ui/input';
 import { Button } from '@chakra-ui/button';
 import { Heading } from '@chakra-ui/layout';
@@ -9,27 +9,28 @@ import { FiHeart } from "react-icons/fi"
 import { FiSearch } from "react-icons/fi"
 import { TbDiscount2 } from "react-icons/tb"
 import { RiShoppingBagLine } from "react-icons/ri"
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { Sidebar } from "./Sidebar";
 import { Logo } from "./Logo";
 
 const Navbar = () => {
 
     let { isOpen, onOpen, onClose } = useDisclosure();
+    let navigate = useNavigate();
 
     const handle_drawer = () => {
         onOpen();
     }
 
     const nav_routes = [
-        { name: "LIPS", path: "" },
-        { name: "EYES", path: "" },
-        { name: "FACE", path: "" },
-        { name: "SKINCARE", path: "" },
-        { name: "ACCESSORIES", path: "" },
-        { name: "GIFT & KITS", path: "" },
-        { name: "BESTSELLERS", path: "" },
-        { name: "NEW LAUNCHES", path: "" },
+        { name: "LIPS", path: "/collections/lips" },
+        { name: "EYES", path: "/collections/eyes" },
+        { name: "FACE", path: "/collections/face" },
+        { name: "SKINCARE", path: "/collections/skincare" },
+        { name: "ACCESSORIES", path: "/collections/accessories" },
+        { name: "GIFT & KITS", path: "/collections/kit" },
+        { name: "BESTSELLERS", path: "/collections/seller" },
+        { name: "NEW LAUNCHES", path: "/collections/new" },
         { name: "OFFERS", path: "" },
         { name: "BLOG", path: "" },
         { name: "STORES", path: "" },
@@ -50,10 +51,10 @@ const Navbar = () => {
                 <Flex display={["flex", "flex", "flex", "none"]} gap="20px">
                     <HiMenuAlt2 color='white' fontSize="20px" onClick={handle_drawer} />
                     <Sidebar onClose={onClose} isOpen={isOpen} />
-                    <Logo h={["25px", "25px", "40px", "40px"]} display={["block", "block", "none", "none"]} />
+                    <Logo onClick={() => { navigate("/") }} h={["25px", "25px", "40px", "40px"]} display={["block", "block", "none", "none"]} />
                 </Flex>
 
-                <Logo h={["25px", "25px", "40px", "40px"]} display={["none", "none", "block", "block"]} />
+                <Logo onClick={() => { navigate("/") }} h={["25px", "25px", "40px", "40px"]} display={["none", "none", "block", "block"]} />
 
                 <Flex w="40%" border="1px solid white" borderRadius="8px" overflow="hidden" display={["none", "none", "none", "flex"]}>
                     <Input
@@ -92,7 +93,7 @@ const Navbar = () => {
                 fontSize="14px"
                 whiteSpace="nowrap">
                 {nav_routes.map(({ name, path }, id) => {
-                    return <NavLink className="nav_item hover-underline-animation " to={path} key={id}> {name} </NavLink>
+                    return <NavLink className="nav_item hover-underline-animation" to={path} key={id}> {name} </NavLink>
                 })}
             </Flex>
         </Flex>
--- src/routes/RouteProvider.jsx
+++ src/routes/RouteProvider.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Box } from "@chakra-ui/react"
 import { Route, Routes } from 'react-router-dom';
 import Home from '../components/Home/Home';
+import Product from '../components/Product/Product';
 
 const RouteProvider = () => {
     return (
         <Box mt={["55px", "55px", "80px", "135px"]} minH="80vh">
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/collections/:product' element={<Product />} />
             </Routes>
         </Box>
 
