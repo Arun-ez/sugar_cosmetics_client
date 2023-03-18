6620e91ccf3e3542659d540e5dd1e3525422ecab
Arun-ez <arunshaw433@gmail.com>
2023-03-19 00:08:20 +0530

    viewfinder route added

--- src/routes/RouteProvider.jsx
+++ src/routes/RouteProvider.jsx
@@ -6,6 +6,7 @@ import Home from '../components/Home/Home';
 import Product from '../components/Product/Product';
 import { Search } from '../components/Search/Search';
 import Loginpage from '../components/Loginpage/loginpage';
+import { ViewFinder } from '../components/ViewFinder/ViewFinder';
 import { GlobalContext } from '../contexts/GlobalContextProvider';
 
 const RouteProvider = ({ ad_display }) => {
@@ -13,12 +14,16 @@ const RouteProvider = ({ ad_display }) => {
     let { isLoginPage } = useContext(GlobalContext);
 
     return (
-        <Box mt={isLoginPage ? ["0px", "0px", "0px", "0px"] : ad_display === "none" ? ["55px", "55px", "80px", "135px"] : ["85px", "85px", "110px", "165px"]} minH="80vh">
+        <Box
+            mt={isLoginPage ? ["0px", "0px", "0px", "0px"] : ad_display === "none" ? ["55px", "55px", "80px", "135px"] : ["85px", "85px", "110px", "165px"]}
+            minH="80vh"
+        >
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/collections/:product' element={<Product />} />
                 <Route path='/search' element={<Search />} />
                 <Route path='/account' element={<Loginpage />} />
+                <Route path='/products/:id' element={<ViewFinder />} />
             </Routes>
         </Box>
 
