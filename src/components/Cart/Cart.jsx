924894f5eca1b4edeb51ea1f3830e45204237be4
Arun-ez <arunshaw433@gmail.com>
2023-07-02 20:04:31 +0530

    fixed date issues

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -62,7 +62,7 @@ const Cart = () => {
         date.setDate(date.getDate() + payload);
 
         const year = date.getFullYear();
-        const month = date.getMonth().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
         const day = date.getDate().toString().padStart(2, '0');
 
         return `${day}-${month}-${year}`;
